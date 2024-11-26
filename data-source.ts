1d29772bd25b4af0b4c2a1edc07972929dd50df3
Payrouse <leonardobone99@gmail.com>
2024-11-26 09:20:58 -0500

    pb de 03

--- data-source.ts
+++ data-source.ts
@@ -1,12 +1,15 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
+import 'reflect-metadata';
 import { DataSource } from 'typeorm';
-
+console.log(process.env.POSTGRES_HOST);
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: '1234',
-  database: 'db_nest_auth',
+  host: process.env.POSTGRES_HOST,
+  port: parseInt(process.env.POSTGRES_PORT),
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
   synchronize: false,
   logging: true,
   entities: ['./src/**/*.entity.ts'],
